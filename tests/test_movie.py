ac967912098e56e018247cbd0eafab3e92ca58ee
svetlana-kuprina <kupsveta@rambler.ru>
2025-10-03 16:40:40 +0500

    Add calculate_taxes.py and test_movie.py function

---
+++ tests/test_movie.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.parametrize("movie_list1, my_genre1, expected1", [(
+                                                                    [{"title": "The Shawshank Redemption",
+                                                                     "genre": "Drama", "director": "Frank Darabont"},
+                                                                    {"title": "The Godfather", "genre": "Crime",
+                                                                     "director": "Francis Ford Coppola"},
+                                                                    {"title": "The Dark Knight", "genre": "Action",
+                                                                     "director": "Christopher Nolan"}],
+                                                                    "Drama",
+                                                                [{'title': 'The Shawshank Redemption', 'genre': 'Drama',
+                                                                  'director': 'Frank Darabont'}]
+)])
+def test_movie(movie_list1, my_genre1, expected1):
+    assert (movie_list1, my_genre1) == expected1
