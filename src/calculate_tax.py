43b384b21eb28c79b98b917bab0291b09f4def3a
svetlana-kuprina <kupsveta@rambler.ru>
2025-10-07 12:17:21 +0500

    Add calculate_tax.py

--- src/calculate_tax.py
+++ src/calculate_tax.py
@@ -1,15 +1,15 @@
-def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
+def calculate_tax(price: list[float], tax_rate: float) -> list[float]:
     """Функция вычисляет стоимость товаров с учётом налога."""
 
-    if tax_rate < 0:
+    if tax_rate < 0 or tax_rate >= 100:
         raise ValueError("Неверный налоговый процент")
 
     taxed_prices = []
 
-    for price in prices:
-        if price <= 0:
+    for pr in price:
+        if pr <= 0:
             raise ValueError("Неверная цена")
-        tax = price * tax_rate / 100
-        taxed_prices.append(price + tax)
+        tax = pr * tax_rate / 100
+        taxed_prices.append(pr + tax)
 
     return taxed_prices
--- tests/test_calculate_taxes.py
+++ tests/test_calculate_taxes.py
@@ -2,7 +2,16 @@ import pytest
 
 from src.calculate_taxes import calculate_taxes
 
-@pytest.mark.parametrize('prices, taxes, expected', [([100, 200, 300], 20, [120, 240, 360]), ([100, 200, 300], 30, [130, 260, 390])])
+@pytest.mark.parametrize('price, tax_rate, expected', [([100, 200, 300], 20, [120, 240, 360]), ([100, 200, 300], 30, [130, 260, 390])])
 
-def test_calculate_taxes(prices, taxes, expected):
-    assert calculate_taxes(prices, taxes) == expected
+def test_calculate_taxes(price, tax_rate, expected):
+    assert calculate_taxes(price, tax_rate) == expected
+
+def test_calculate_taxes_raises_error():
+    with pytest.raises(ValueError) as excinfo:
+        calculate_taxes([100, 200, 300], -10)
+        assert str(excinfo) == ValueError("Неверный налоговый процент")
+
+    with pytest.raises(ValueError) as exc_info:
+        calculate_taxes([0, -10, 300], 10)
+        assert str(excinfo) == ValueError("Неверная цена")
